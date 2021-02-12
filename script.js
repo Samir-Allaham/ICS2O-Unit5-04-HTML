19ca5dc7c656dec205384c7adb7b4ed576882504
SamirAllaham <samir.allaham@ocsbstudent.ca>
2021-02-12 14:11:36 +0000

    fix functions and added button

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ document.getElementById('click-me').addEventListener('click', secondButton)
 
 document.getElementById('button-two').addEventListener('click', buttonOn)
 
+document.getElementById('report').addEventListener ('click', specialButton)
+function specialButton () {
+  document.getElementById('paragraph').innerHTML = 'Done!'
+  document.getElementById('picture').src =
+  './images/img1.jpg'
+  document.getElementById('background').style.backgroundColor = 'grey'}
+
+
 function secondButton () {
   document.getElementById('paragraph').innerHTML = 'Done!'
   document.getElementById('picture').src =
@@ -22,14 +30,6 @@ document.getElementById('click-me').addEventListener('click', secondButton)
 
 document.getElementById('button-two').addEventListener('click', buttonOn)
 
-function thirdButton () {
-  document.getElementById('picture').style.display='inline'
-  document.getElementById('paragraph').innerHTML = 'Done!'
-  document.getElementById('picture').src =
-  './images/AlfredAhoPortrait.jpg'
-  document.getElementById('background').style.backgroundColor = 'green'
-  document.getElementById('paragraph').display = 'none'
-}
 
 document.getElementById('button-three').addEventListener('click', thirdButton)
 
